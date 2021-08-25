7126205a0543d5113556ba9f350285a8e81855e2
nibuk <nibeshkhadka@gmail.com>
2021-08-25 21:47:24 +0300

    Backend logic

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express'),
+    cors = require('cors'),
+    app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const requestLogger = (req, res, next) => {
+    console.log('Method:', req.method)
+    console.log('Path:  ', req.path)
+    console.log('Body:  ', req.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "2019-05-30T17:30:31.098Z",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2019-05-30T18:39:34.091Z",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2019-05-30T19:20:14.298Z",
+        important: true
+    }
+]
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    const note = notes.find(note => note.id === parseInt(id))
+    if (note) {
+        res.json(note)
+    }
+    else {
+        res.status(404).send()
+    }
+});
+
+
+
+app.post('/api/notes', (req, res) => {
+    const note = req.body
+    console.log(note)
+    res.json(note)
+})
+app.delete('/api/notes/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    res.status(204).end()
+})
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
